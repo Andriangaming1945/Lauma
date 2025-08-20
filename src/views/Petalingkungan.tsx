a72a6e37189d38969becea4dd68e2a9702a38fe6
Tartaglia <anrianwibowo06@gmail.com>
2025-08-21 02:26:09 +0700

    add vercel config for routing

--- src/views/Petalingkungan.tsx
+++ src/views/Petalingkungan.tsx
@@ -5,6 +5,10 @@ const Guide: React.FC = () => {
   return (
     <div>
        <Interaktif />
+       {/* The Interaktif component is used here, but you can replace it with any other component
+           or content that you want to display in this view. */}
+       {/* If you want to add more components or content, you can do so here. */}
+       {/* For example, you could add a header, footer, or any other section as needed. */}
     </div>
   );
 };
